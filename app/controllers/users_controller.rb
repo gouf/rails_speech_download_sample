135fe4d289bda91a382e298bd4f3bfdc2837ea02
gouf <innocent.zero@gmail.com>
2015-02-11 20:41:38 +0900

    Add speech download action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def download_speech
+    words = params[:words][:words]
+    lang  = params[:lang]
+    basename = 'speech'
+    tempfile = Tempfile.open([basename, '.mp3'])
+    words.to_file(lang, tempfile.path)
+    send_file(tempfile.path)
+  end
+
   # GET /users/1
   # GET /users/1.json
   def show
